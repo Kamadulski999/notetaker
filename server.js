5c67600f1185620de6a6adf241bc723e344b35bc
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-07-31 06:56:39 -0500

    MVP

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const path = require('path');
-const note = require ('./db/db.json')
+const notes = require ('./db/db.json')
+const { createNote, deleteNote } = require('./lib/createDelete');
+
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 
-// get goes here
+
+// Routes
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
     });
 
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+    });
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+  let note = createNote(req.body, notes);
+  res.json(notes);
+    });    
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
     });
-
+    
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
